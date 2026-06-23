5498450c2791dc8c7b3356dff21cc38e6d3ba3e2
furkan210807 <furkan210807@gmail.com>
2026-06-23 22:50:26 +0530

    add new file

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,34 @@
-import OpenAI from 'openai';
-import 'dotenv/config';
+// import OpenAI from 'openai';
+// import 'dotenv/config';
 
-const client = new OpenAI({
-  apiKey: process.envOPENAI_API_KEY, 
+// const client = new OpenAI({
+//   apiKey: process.envOPENAI_API_KEY, 
+// });
+
+// const response = await client.responses.create({
+//   model: 'gpt-5.4-mini',
+//   input: 'Joke related to computer Science',
+// });
+
+// console.log(response.output_text);
+
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(cors());
+app.listen(PORT,()=>{
+  console.log(`Port is Listening ${PORT}`);
 });
 
-const response = await client.responses.create({
-  model: 'gpt-5.4-mini',
-  input: 'Joke related to computer Science',
-});
+app.post("/test", async(req,res)=>{
+      try{
+        fetch("https://api.openai.com/v1/chat/completions")
 
-console.log(response.output_text);
+        }catch(err){
+        console.log(err);
+        }
+})
