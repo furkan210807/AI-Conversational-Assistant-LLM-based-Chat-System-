35e3e0c783626a350ab8285841b1dca740f6e387
furkan210807 <furkan210807@gmail.com>
2026-06-26 22:51:27 +0530

    add model

--- Backend/server.js
+++ Backend/server.js
@@ -25,8 +25,26 @@ app.listen(PORT,()=>{
 });
 
 app.post("/test", async(req,res)=>{
+    const options  = {
+      method : "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+      },
+      body :JSON.stringify({
+        model : "gpt-5.4-mini",
+        messages : [{
+           role: "user",
+           content: req.body.message
+        }]
+      })
+    }
       try{
-        fetch("https://api.openai.com/v1/chat/completions")
+       const response = await fetch("https://api.openai.com/v1/chat/completions");
+       const data = await response.json();
+      //  console.log(data.choices[0].message.content); // reply 
+       res.send(data.choices[0].message.content);
+     
 
         }catch(err){
         console.log(err);
