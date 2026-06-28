c8ec67190948d3316e75a95591554e8d1417cc6f
furkan210807 <furkan210807@gmail.com>
2026-06-28 12:59:52 +0530

    Add frontend

---
+++ Frontend/src/ChatWindow.jsx
@@ -0,0 +1,9 @@
+import "./ChatWindow.css";
+function ChatWindow(){
+    return (
+        <div>
+            ChatWindow
+        </div>
+    )
+}
+export default ChatWindow;
---
+++ Frontend/src/Sidebar.jsx
@@ -0,0 +1,25 @@
+import "./Sidebar.css"
+function Sidebar(){
+    return(
+        <section className="sidebar">
+            {/*new chat Button*/}
+            <button>
+                <img src="https://imgs.search.brave.com/nGld_ROSRtP-EAQeEttK9iuxrd98dDnt7dB2EyFhN5s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvcHJl/dmlld3MvMDIxLzYw/OC83OTAvbm9uXzJ4/L2NoYXRncHQtbG9n/by1jaGF0LWdwdC1p/Y29uLW9uLWJsYWNr/LWJhY2tncm91bmQt/ZnJlZS12ZWN0b3Iu/anBn" alt="gpt logo" />
+               <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+            {/*history*/}
+            <ul className="history">
+                <li>History1</li>
+                <li>History2</li>
+                <li>History3</li>
+               
+            </ul>
+            {/*sign*/}
+            <div className="sign">
+                <p>By Apna College &hearts;</p>
+            </div>
+        </section>
+    )
+}
+
+export default Sidebar;
